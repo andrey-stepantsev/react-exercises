c6db3066d9283b480de063dc866f3771dc5e9ac1
Andrey Stepantsev <67436206+andrey-stepantsev@users.noreply.github.com>
2020-08-09 23:58:00 +0300

    added some configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/App.tsx",
+  entry: ["@babel/polyfill", "./src/App.tsx"],
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "dist"),
